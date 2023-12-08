dee57b6cbee0c43d59d83a1b5074edd32d30375b
Artem Radchenko <sdad@bk.ru>
2023-12-09 01:03:04 +0300

    Seminar001 Task003 WeekDay

---
+++ Seminar001/Task003/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу которая будет выдавать название дня недели по заданному номеру.
+
+Console.Write("Enter week day (1-7): ");
+string weekDay = Console.ReadLine();
+int numWeekDay = Convert.ToInt32(weekDay);
+if(numWeekDay == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if(numWeekDay == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if(numWeekDay == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if(numWeekDay == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if(numWeekDay == 5)
+{
+    Console.WriteLine("Friday");
+}
+else if(numWeekDay == 6)
+{
+    Console.WriteLine("Saturday");
+}
+else if(numWeekDay == 7)
+{
+    Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("uncorect week day");
+}
