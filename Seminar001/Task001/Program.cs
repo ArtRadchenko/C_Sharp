d9a237d908c7a10fe6509f61f53ad7bb45d6e55d
Artem Radchenko <sdad@bk.ru>
2023-12-08 23:30:19 +0300

    Seminar001 Tasck 001 Square

---
+++ Seminar001/Task001/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает два целых числа и выдает его квадрат (число умноженное само на себя).
+
+System.Console.Write("Enter number: ");
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str);
+
+int square = num * num;
+
+System.Console.WriteLine($"square = {square}");
