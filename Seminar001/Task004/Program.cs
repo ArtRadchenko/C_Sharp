d32f312405d5104d95998ff06dc7b4a65244d017
Artem Radchenko <sdad@bk.ru>
2023-12-09 13:13:26 +0300

    Seminar001 Task004 While from to

---
+++ Seminar001/Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу которая на вход принимает целое число N, а на выходе показывает все целые числа от -N до N.
+
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str);
+int numStart = -num;
+while(numStart <= num)
+{
+    Console.WriteLine($"{numStart}");
+    ++numStart;
+}
