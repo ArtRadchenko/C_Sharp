2d87c08af7aa09eaa34af87dcd5a0c80d2889463
Artem Radchenko <sdad@bk.ru>
2023-12-10 02:59:09 +0300

    Add Seminar 002 & Task001

---
+++ Seminar002/Task001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу которая берет случайное число из отрезка 10-99 и выводин наибольшую цифру из него.
+Console.WriteLine("Hello, World!");
+
+int number = GetNumber();
+System.Console.WriteLine(number);
+
+int num1 = number / 10;
+int num2 = number % 10;
+
+if(num1 > num2)
+{
+    System.Console.WriteLine(num1);
+}
+else
+{
+    System.Console.WriteLine(num2);
+}
+
+int GetNumber()
+{
+    Random rand = new Random();
+    int num = rand.Next(10, 100);
+    return num;
+}
