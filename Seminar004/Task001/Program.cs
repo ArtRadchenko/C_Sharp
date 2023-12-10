c01193ca2ae85cfb7cead7441adfbe88e7ae26d9
Artem Radchenko <sdad@bk.ru>
2023-12-10 14:11:35 +0300

    Seminar 004

---
+++ Seminar004/Task001/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Определите количество простых чисел в этом массиве. Примеры [1 3 4 19 3] => 2 [4 3 4 1 9 5 21 13]  => 3
+int[] list = new int[10];
+void FillArray(int[]collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+FillArray(list);
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+PrintArray(list);
+bool IsPrime(int num)
+{
+    for(int i = 2; i < num; i++)
+    {
+        if(num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int GetCountPrimeNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(IsPrime(array[i]))
+        {
+            count++;
+
+        }
+    }
+    return count;
+}
+Console.WriteLine();
+Console.WriteLine(GetCountPrimeNumber(list));
---
+++ Seminar004/Task002/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с клавиатуры). Найдите количество чисел, которые оканчиваются на 1 и делятся нацело на 7. Пример [1 5 11 21 81 4 0 91 2 3] => 2
+int[] FillArray2(int num)
+{
+    int[] numbers = new int[num];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 100);
+    }
+    return numbers;
+}
+
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+}
+
+bool IsCheck(int num)
+{
+    return ((num % 7 == 0) && (num % 10 == 1));
+}
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsCheck(array[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] list = FillArray2(size);
+PrintArray(list);
+Console.WriteLine();
+Console.WriteLine(GetCount(list));
