1fb97011f40dd5139daa5713d85ed96f90c514a7
Artem Radchenko <sdad@bk.ru>
2023-12-10 05:13:13 +0300

    Seminar 002 Home Work 002

---
+++ Seminar002/HomeWork/HW002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+Console.WriteLine("Введите координату X:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > 0 && y > 0) {Console.WriteLine("Первая четверть");}
+if(x < 0 && y > 0) {Console.WriteLine("Вторая четверть");}
+if(x < 0 && y < 0) {Console.WriteLine("Третья четверть");}
+if(x > 0 && y < 0) {Console.WriteLine("Четвертая четверть");}
