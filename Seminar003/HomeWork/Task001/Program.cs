d8a48a67442f6c1088db547ee79b807559eca296
Artem Radchenko <sdad@bk.ru>
2023-12-14 01:32:55 +0300

    Seminar 003 Home Work Task 001

---
+++ Seminar003/HomeWork/Task001/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90]
+
+int[] RandomFillArray(int num)
+{
+    int[] numbers = new int[num];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 100);
+    }
+    return numbers;
+}
+
+bool IsCheck(int num)
+{
+    return ((num > 19) && (num < 91));
+}
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsCheck(array[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] list = RandomFillArray(size);
+PrintArray(list);
+Console.WriteLine();
+Console.Write("Ответ: ");
+Console.WriteLine(GetCount(list));
