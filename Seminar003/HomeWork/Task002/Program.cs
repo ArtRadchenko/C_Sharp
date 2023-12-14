10309c030e867c4cd7ad3185258c94eb23e3da85
Artem Radchenko <sdad@bk.ru>
2023-12-14 22:29:04 +0300

    Seminar 003 Home Work Task 002

---
+++ Seminar003/HomeWork/Task002/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] RandomFillArray(int num)
+{
+    int[] numbers = new int[num];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 100);
+    }
+    return numbers;
+}
+
+bool IsCheck(int num)
+{
+    return (num % 2 == 0);
+}
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsCheck(array[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+}
+
+Console.Write("Массив: ");
+int size = 10;
+int[] list = RandomFillArray(size);
+PrintArray(list);
+Console.Write("\nОтвет: ");
+Console.WriteLine(GetCount(list));
