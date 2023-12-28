09744bccf46ae5b6349fb27841f87ef214582f5f
Artem Radchenko <sdad@bk.ru>
2023-12-29 01:54:45 +0300

    Seminar 007

---
+++ Seminar007/Task003/Program.cs
@@ -0,0 +1,30 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. 
+// Вывести на экран согласные буквы этой строки.
+
+string stroka = GetString();
+PrintString(stroka);
+
+string GetString()
+{
+    Console.WriteLine("Введите строку: ");
+    string list = Console.ReadLine();
+    return list;
+}
+
+bool IsVowel(char c)
+{
+    return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+}
+
+void PrintString(string str, int index = 0)
+{
+    if (index < str.Length)
+    {
+        char c = Char.ToUpper(str[index]);
+        if ((c >= 'A' && c <= 'Z') && !IsVowel(c)) // ! - превращает истину в лож, а лож - в истину
+        {
+            Console.Write(c + " ");
+        }
+        PrintString(str, index + 1);
+    }
+}
